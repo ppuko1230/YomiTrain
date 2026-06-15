95e17a009c02ea2e47abd6fac683be078debde22
unknown <hotkafeore@gmail.com>
2026-06-15 19:32:11 +0900

    [Add]画面遷移機能を追加しました

---
+++ Assets/Scripts/UI/Manager/TitleUIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class TitleUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject titlePanel;
+    [SerializeField] private GameObject roomSelectionPanel;
+    [SerializeField] private GameObject settingsPanel;
+
+    // 開始ボタン
+    public void ShowRoomSelection()
+    {
+        titlePanel.SetActive(false);
+        roomSelectionPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    // 設定ボタン
+    public void ShowSettings()
+    {
+        titlePanel.SetActive(false);
+        roomSelectionPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // 戻るボタン
+    public void ReturnToTitle()
+    {
+        titlePanel.SetActive(true);
+        roomSelectionPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+}
